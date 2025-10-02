a4336de2c10e3fc980a689426392a7d66e562da9
AxellGuillen <arianowsky@gmail.com>
2025-10-02 14:18:16 -0600

    feat: cambios en layout, navegación y páginas nuevas

---
+++ pages/servicios.vue
@@ -0,0 +1,19 @@
+<script setup>
+const pageTitle = computed(() => `Servicios - Servicios Centro Holístico`);
+
+useHead({
+  title: pageTitle,
+  meta: [{ name: "description", content: "Art and high fashion" }],
+});
+</script>
+
+<template>
+  <div
+    class="h-screen flex flex-col justify-center items-center w-full bg-pink-200"
+  >
+    <h1 class="text-black font-headlines text-8xl">Servicios</h1>
+    <NuxtLink to="/" prefetch>
+      <h1 class="text-4xl text-black">Ir a Home</h1>
+    </NuxtLink>
+  </div>
+</template>
---
+++ pages/viajesTerapeuticos.vue
@@ -0,0 +1,19 @@
+<script setup>
+const pageTitle = computed(() => `Viajes Terapéuticos - Viajes`);
+
+useHead({
+  title: pageTitle,
+  meta: [{ name: "description", content: "Art and high fashion" }],
+});
+</script>
+
+<template>
+  <div
+    class="h-screen flex flex-col justify-center items-center w-full bg-pink-200"
+  >
+    <h1 class="text-black font-headlines text-8xl">Viajes Terapéuticos</h1>
+    <NuxtLink to="/" prefetch>
+      <h1 class="text-4xl text-black">Ir a Home</h1>
+    </NuxtLink>
+  </div>
+</template>
